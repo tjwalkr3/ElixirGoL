8dd0c9f79c2869104a370d41e178ff8a5bafc8bc
Thomas Jones <tjwalkr3@gmail.com>
2024-11-03 13:48:07 -0700

    added some stuff

--- geometry.ex
+++ geometry.ex
@@ -1,9 +1,30 @@
 defmodule Geometry do
-  def rectangle_area(a, b) do
-    a * b
+  defmodule Rectangle do
+    def rectangle_area(a, b) do
+      a * b
+    end
+
+    def print_5 do
+        -5
+        |> abs()
+        |> Integer.to_string()
+        |> IO.puts()
+    end
+  end
+
+  defmodule Circle do
+    @pi 3.14159 # This is how attributes are set
+
+    def area(r), do: r*r*@pi
+    def circumference(r), do: 2*r*@pi
   end
 end
 
 # Use this function by running
-# Geometry.rectangle_area(6, 7)
+# iex geometry.ex
+# Geometry.Rectangle.rectangle_area(6, 7)
+# Geometry.Rectangle.print_5
 # in the terminal
+
+# if a function has no parameters, you can omit the parentheses
+# use "mix format geometry.ex" to format your code
